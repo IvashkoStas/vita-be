f66a3460bcaa80da8dba86cfa6d715efd80f385c
Aspire212 <1ven970@gmail.com>
2023-10-26 17:54:03 +0300

    update configservice

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -1,7 +1,5 @@
 import * as dotenv from 'dotenv';
 import { injectable } from 'inversify';
-import fs from 'node:fs';
-import path from 'node:path';
 import { IConfigService } from './config.service.interface';
 
 @injectable()
@@ -9,16 +7,10 @@ export class ConfigService implements IConfigService {
   private readonly envConfig: { [key: string]: unknown };
 
   constructor() {
-      const envFilePath = path.join(process.cwd(), '.env');
-      const existsPath = fs.existsSync(envFilePath);
-      if (!existsPath) {
-        console.log('.env file does not exist', envFilePath);
-        process.exit(0);
-      }
-      this.envConfig = dotenv.parse(fs.readFileSync(envFilePath));
+      dotenv.config();
   }
 
   get<T>(key: string): T {
-    return this.envConfig[key] as T;
+    return process.env[key] as T;
   }
 }
